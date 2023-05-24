9ba775d5f650a2276b36b0c926882fb4f84921f7
ohprettyhak <haklee.googl@gmail.com>
2023-05-24 11:08:49 +0900

    feat: add cl-r4-parser assignment project

---
+++ Computer and Lingustics/cl-r4-parser/main.py
@@ -0,0 +1,29 @@
+import nltk
+import time
+
+
+def get_sentences():
+    with open("data/processed/sentence.txt", "r") as f:
+        return f.read().splitlines()
+
+
+def run(start_time, sentences):
+    for idx, sentence in enumerate(sentences):
+        grammar = nltk.data.load("config/grammar.cfg")
+        parser = nltk.ChartParser(grammar, trace=2)
+        tokens = nltk.tokenize.word_tokenize(sentence)
+        print(tokens)
+
+        trees = parser.parse(tokens)
+
+        out = open(f"data/output/{idx}_{start_time}.txt", "w")
+
+        for tree in trees:
+            tree.pretty_print()
+            tree.draw()
+            out.write(str(tree) + "\n\n")
+        out.close()
+
+
+if __name__ == '__main__':
+    run(time.time_ns(), ['The company is looking for competent employees.'])
